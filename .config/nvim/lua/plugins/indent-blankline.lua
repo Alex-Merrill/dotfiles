0ed13ca81e38ee4218a69a61f3d66824c19dee45
Alex-Merrill <aomerrill98@gmail.com>
2022-09-28 03:24:05 -0400

    disable indent lines for text files

--- .config/nvim/lua/plugins/indent-blankline.lua
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -10,6 +10,9 @@ vim.opt.list = true
 vim.opt.listchars:append "eol:↴"
 
 require("indent_blankline").setup {
+  filetype_exclude = {
+    "text",
+  },
   show_current_context = true,
   show_current_context_start = true,
   char_highlight_list = {
